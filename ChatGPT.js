1e25aaa313d52d9675327578e5d4ab2de1e295ad
quoc-trung-nguyen-hcmus <21120350@student.hcmus.edu.vn>
2023-03-20 13:45:09 +0700

    yes
    yes

--- ChatGPT.js
+++ ChatGPT.js
@@ -1,4 +1,4 @@
-var OPENAI_API_KEY = "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";
+var OPENAI_API_KEY = "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";
 var text_to_speech_is_supported = false;
 var speech_is_in_progress = false;
 var speech_recognizer = null
@@ -7,7 +7,7 @@ var voice_input = null;
 
 // ???c g?i ngay khi trang web ???c t?i 
 function OnLoad() {
-    // Ki?m tra trình duy?t có h? tr? nh?n d?ng gi?ng nói không
+    // Ki?m tra trï¿½nh duy?t cï¿½ h? tr? nh?n d?ng gi?ng nï¿½i khï¿½ng
     if ("webkitSpeechRecognition" in window) {
     }
     else {
@@ -15,11 +15,11 @@ function OnLoad() {
         lblSpeak.style.display = "none";
     }
 
-    // Ki?m tra trình duy?t có h? tr? chuy?n ??i v?n b?n thành gi?ng nói không
+    // Ki?m tra trï¿½nh duy?t cï¿½ h? tr? chuy?n ??i v?n b?n thï¿½nh gi?ng nï¿½i khï¿½ng
     if ('speechSynthesis' in window) {
         text_to_speech_is_supported = true;
         speechSynthesis.onvoiceschanged = function () {
-            // Thay ??i gi?ng nói theo tùy ch?nh c?a ng??i dùng
+            // Thay ??i gi?ng nï¿½i theo tï¿½y ch?nh c?a ng??i dï¿½ng
             voice_input = window.speechSynthesis.getVoices();
             for (var i = 0; i < voice_input.length; i++) {
                 selVoices[selVoices.length] = new Option(voice_input[i].name, i);
@@ -28,7 +28,7 @@ function OnLoad() {
     }
 }
 
-// Thay ??i ngôn ng? c?a âm thanh ??u vào
+// Thay ??i ngï¿½n ng? c?a ï¿½m thanh ??u vï¿½o
 function ChangeLang(o) {
     if (speech_recognizer) {
         speech_recognizer.lang = selLang.value;
@@ -41,16 +41,16 @@ function ChangeLang(o) {
 const conversation_history = []
 
 
-// Hàm Send là back-end
+// Hï¿½m Send lï¿½ back-end
 function Send() {
     // str_input: input text ??n thu?n
     // chat-input: ??i t??ng input
     var str_input = $("#chat-input").val();
 
 
-    if (str_input == "") { // Báo l?i và thoát kh?i ch??ng trình n?u prompt r?ng
+    if (str_input == "") { // Bï¿½o l?i vï¿½ thoï¿½t kh?i ch??ng trï¿½nh n?u prompt r?ng
         alert("Type in your question!");
-        chat-input.focus(); // ??t con tr? so?n th?o vào h?p nh?p
+        chat-input.focus(); // ??t con tr? so?n th?o vï¿½o h?p nh?p
         return;
     }
 
@@ -64,7 +64,7 @@ function Send() {
                      <div>
                          <p class="mb-0 opacity-75">${str_input}</p>
                      </div>
-                     <img src="images/Logo_HCMUS.png" alt = "twbs" width = "32" height = "32" class="rounded-circle flex-shrink-0">
+                     <img src="../images/Logo_HCMUS.png" alt = "twbs" width = "32" height = "32" class="rounded-circle flex-shrink-0">
                  </div>
               </a>
             `;
@@ -73,102 +73,102 @@ function Send() {
     //Append prompt text to history
     $("#list-group").append(html_data);
 
-    // G?i yêu c?u d? li?u ??n máy ch?
+    // G?i yï¿½u c?u d? li?u ??n mï¿½y ch?
     var oHttp = new XMLHttpRequest();
 
-    oHttp.open("POST", "https://api.openai.com/v1/chat/completions"); // Máy ch? ?? g?i yêu c?u POST
+    oHttp.open("POST", "https://api.openai.com/v1/chat/completions"); // Mï¿½y ch? ?? g?i yï¿½u c?u POST
 
-    // Header c?a yêu c?u
-    oHttp.setRequestHeader("Accept", "application/json"); // Yêu c?u ??u vào ch? ch?p nh?n môi tr??ng javascrpt
-    oHttp.setRequestHeader("Content-Type", "application/json"); // K?t qu? ??u vào tuân theo  cú pháp javascript
-    oHttp.setRequestHeader("Authorization", "Bearer " + OPENAI_API_KEY) // Xác th?c truy c?p, s? d?ng API c?a OpenAI
+    // Header c?a yï¿½u c?u
+    oHttp.setRequestHeader("Accept", "application/json"); // Yï¿½u c?u ??u vï¿½o ch? ch?p nh?n mï¿½i tr??ng javascrpt
+    oHttp.setRequestHeader("Content-Type", "application/json"); // K?t qu? ??u vï¿½o tuï¿½n theo  cï¿½ phï¿½p javascript
+    oHttp.setRequestHeader("Authorization", "Bearer " + OPENAI_API_KEY) // Xï¿½c th?c truy c?p, s? d?ng API c?a OpenAI
 
     conversation_history[conversation_history.length] = { "role": "user", "content": str_input };
-    // D? li?u trong yêu c?u ???c g?i lên máy ch?
+    // D? li?u trong yï¿½u c?u ???c g?i lï¿½n mï¿½y ch?
     var data = {
-        "model": "gpt-3.5-turbo", // Model GPT-3.5-turbo, model hi?n hành c?a ChatGPT
-        "messages": conversation_history, // Thông ?i?p ?? g?i API ph?n h?i
+        "model": "gpt-3.5-turbo", // Model GPT-3.5-turbo, model hi?n hï¿½nh c?a ChatGPT
+        "messages": conversation_history, // Thï¿½ng ?i?p ?? g?i API ph?n h?i
     }
 
-    // Truy?n thông ?i?p c?a ng??i dùng vào stream xu?t
+    // Truy?n thï¿½ng ?i?p c?a ng??i dï¿½ng vï¿½o stream xu?t
     txtOutput = '';
     txtOutput += "User: " + str_input + "\n"
 
 
 
-    // ??nh ngh?a hành vi x?y ra n?u tr?ng thái c?a bi?n oHttp thay ??i (??ng ngh?a v?i vi?c ?ã nh?n d? li?u t? máy ch?)
-    // (ch?a bi?t là d? li?u ?úng hay không)
+    // ??nh ngh?a hï¿½nh vi x?y ra n?u tr?ng thï¿½i c?a bi?n oHttp thay ??i (??ng ngh?a v?i vi?c ?ï¿½ nh?n d? li?u t? mï¿½y ch?)
+    // (ch?a bi?t lï¿½ d? li?u ?ï¿½ng hay khï¿½ng)
 
-    // Th?c thi kh?i l?nh sau khi tr?ng thái oHttp thay ??i:
+    // Th?c thi kh?i l?nh sau khi tr?ng thï¿½i oHttp thay ??i:
     oHttp.onreadystatechange = function () {
 
-        // XMLHttpRequest.readyState cho bi?t tr?ng thái  yêu c?u ?ã g?i t? máy khách.
-        // Giá tr? 0 (ch?a ti?n hành) -> 4 (?ã hoàn thành) bi?u th? m?c ?? hoàn thành c?a yêu c?u. 
+        // XMLHttpRequest.readyState cho bi?t tr?ng thï¿½i  yï¿½u c?u ?ï¿½ g?i t? mï¿½y khï¿½ch.
+        // Giï¿½ tr? 0 (ch?a ti?n hï¿½nh) -> 4 (?ï¿½ hoï¿½n thï¿½nh) bi?u th? m?c ?? hoï¿½n thï¿½nh c?a yï¿½u c?u. 
         if (oHttp.readyState === 4) {
-            var oJson = {} // oJson l?u d? li?u ???c máy ch? g?i v?
+            var oJson = {} // oJson l?u d? li?u ???c mï¿½y ch? g?i v?
 
-            // N?u stream output r?ng, thêm ký t? xu?ng dòng vào stream.
+            // N?u stream output r?ng, thï¿½m kï¿½ t? xu?ng dï¿½ng vï¿½o stream.
             if (txtOutput != "") txtOutput += "\n";
 
-            // Test dòng l?nh sau và tr? v? thông báo l?i (n?u có)
+            // Test dï¿½ng l?nh sau vï¿½ tr? v? thï¿½ng bï¿½o l?i (n?u cï¿½)
             try {
-                // L?u oHttp.responseText (d? li?u ???c máy ch? tr? v?) vào bi?n oJson
+                // L?u oHttp.responseText (d? li?u ???c mï¿½y ch? tr? v?) vï¿½o bi?n oJson
                 oJson = JSON.parse(oHttp.responseText);
             }
-            catch (ex) // N?u quá trình parse không thành công: 
+            catch (ex) // N?u quï¿½ trï¿½nh parse khï¿½ng thï¿½nh cï¿½ng: 
             {
-                // Bi?n ex t?m th?i l?u thông báo l?i
-                txtOutput += "Error: " + ex.message + "\n" // L?u thông báo l?i vào stream output.
+                // Bi?n ex t?m th?i l?u thï¿½ng bï¿½o l?i
+                txtOutput += "Error: " + ex.message + "\n" // L?u thï¿½ng bï¿½o l?i vï¿½o stream output.
             }
 
-            if (oJson.error && oJson.error.message) // N?u có l?i x?y ra
+            if (oJson.error && oJson.error.message) // N?u cï¿½ l?i x?y ra
             {
-                txtOutput += "Error: " + oJson.error.message + "\n"; // L?u thông ?i?p l?i vào k?t qu? ??u ra
+                txtOutput += "Error: " + oJson.error.message + "\n"; // L?u thï¿½ng ?i?p l?i vï¿½o k?t qu? ??u ra
             }
-            else  // Không có l?i, quá trình parse thành công
+            else  // Khï¿½ng cï¿½ l?i, quï¿½ trï¿½nh parse thï¿½nh cï¿½ng
                 if (oJson.choices && oJson.choices[0].message.content) {
-                    // s l?u k?t qu? d??i d?ng v?n b?n thô 
+                    // s l?u k?t qu? d??i d?ng v?n b?n thï¿½ 
                     var s = oJson.choices[0].message.content
 
-                    // N?u k?t qu? v?n b?n là r?ng, thông báo
+                    // N?u k?t qu? v?n b?n lï¿½ r?ng, thï¿½ng bï¿½o
                     if (s == "")
                         s = "No response";
 
-                    // Truy?n k?t qu? v?n b?n vào stream ??u ra
+                    // Truy?n k?t qu? v?n b?n vï¿½o stream ??u ra
                     txtOutput += "Chat GPT: " + s + '\n';
-                    s.replace("\n", "<br>");
+                    s = s.replace(/\n/g, "<br>");
                     let gpt_data = '';
                     gpt_data += `
                     <a href = "#" class="list-group-item list-group-item-action d-flex gap-3 py-3">
-                        <img src = "images/openai.jpg" alt = "twbs" width = "32" height = "32" class="rounded-circle flex-shrink-0">
+                        <img src = "../images/openai.jpg" alt = "twbs" width = "32" height = "32" class="rounded-circle flex-shrink-0">
                         <div class="d-flex gap-2 w-100 justify-content-start">
-                            <div>
-                             <p class="mb-0 opacity-75"> ${s} </p>
+                            <div style="text-align: left;">
+                             <span class="mb-0 opacity-75"> ${s} </span>
                             </div>
                         </div>
                     </a>
                     `;
                     $("#list-group").append(gpt_data);
 
-                    // L?u k?t qu? vào l?ch s?
+                    // L?u k?t qu? vï¿½o l?ch s?
                     conversation_history[conversation_history.length] = { "role": "assistant", "content": s };
-                    // Nói
+                    // Nï¿½i
                     TextToSpeech(s);
                 }
         }
     };
     
 
-    // Chuy?n data thành d?ng v?n b?n và g?i yêu c?u ?i
+    // Chuy?n data thï¿½nh d?ng v?n b?n vï¿½ g?i yï¿½u c?u ?i
     oHttp.send(JSON.stringify(data));
 
-    // N?u stream xu?t có d? li?u, thêm ký t? xu?ng dòng vào stream
+    // N?u stream xu?t cï¿½ d? li?u, thï¿½m kï¿½ t? xu?ng dï¿½ng vï¿½o stream
     if (txtOutput != "")
         txtOutput += "\n"
 
 
 
-    // D?n d?p prompt ??u vào
+    // D?n d?p prompt ??u vï¿½o
     $("#chat-input").val() = "";
 }
 
@@ -177,55 +177,55 @@ function Send() {
 
 
 
-// Ph??ng th?c chuy?n ??i thông ?i?p v?n b?n thành gi?ng nói
+// Ph??ng th?c chuy?n ??i thï¿½ng ?i?p v?n b?n thï¿½nh gi?ng nï¿½i
 function TextToSpeech(s) {
-    // N?u không có h? tr? gi?ng nói, thoát.
+    // N?u khï¿½ng cï¿½ h? tr? gi?ng nï¿½i, thoï¿½t.
     if (text_to_speech_is_supported == false)
         return;
 
-    // N?u âm thanh ??u ra b? t?t, thoát.
+    // N?u ï¿½m thanh ??u ra b? t?t, thoï¿½t.
     if (chkMute.checked) return;
 
-    // T?o ??i t??ng c?a Web Speech API ?? ti?n hành g?i & nh?n yêu c?u chuy?n ??i gi?ng nói <---> v?n b?n  
+    // T?o ??i t??ng c?a Web Speech API ?? ti?n hï¿½nh g?i & nh?n yï¿½u c?u chuy?n ??i gi?ng nï¿½i <---> v?n b?n  
     speech_utterance = new SpeechSynthesisUtterance();
 
-    // ??i t??ng voice_input l?u tr? âm thanh ??u vào (???c ng??i dùng nh?p t? microphone)
+    // ??i t??ng voice_input l?u tr? ï¿½m thanh ??u vï¿½o (???c ng??i dï¿½ng nh?p t? microphone)
     if (voice_input) {
-        // N?u có âm thanh ??u vào ???c l?u b?i ??i t??ng selVoice, l?u âm thanh vào bi?n sVoice
+        // N?u cï¿½ ï¿½m thanh ??u vï¿½o ???c l?u b?i ??i t??ng selVoice, l?u ï¿½m thanh vï¿½o bi?n sVoice
         var sVoice = selVoices.value;
-        if (sVoice != "") // N?u có âm thanh, l?y gi?ng t??ng ?ng ?? truy?n vào bi?n ph?n h?i
+        if (sVoice != "") // N?u cï¿½ ï¿½m thanh, l?y gi?ng t??ng ?ng ?? truy?n vï¿½o bi?n ph?n h?i
         {
             speech_utterance.voice = voice_input[parseInt(sVoice)];
         }
     }
 
 
-    // ??nh ngh?a hành ??ng s? th?c thi khi máy ?ã nói xong
+    // ??nh ngh?a hï¿½nh ??ng s? th?c thi khi mï¿½y ?ï¿½ nï¿½i xong
     speech_utterance.onend = function () {
-        // N?u có âm thanh ??u vào t? ng??i dùng, b?t ??u thu âm
+        // N?u cï¿½ ï¿½m thanh ??u vï¿½o t? ng??i dï¿½ng, b?t ??u thu ï¿½m
         if (speech_recognizer && chkSpeak.checked) {
             speech_recognizer.start();
         }
     }
 
-    // N?u máy ?ang nói mà mic ?ang m?
+    // N?u mï¿½y ?ang nï¿½i mï¿½ mic ?ang m?
     if (speech_recognizer && chkSpeak.checked) {
-        // Không thu âm
+        // Khï¿½ng thu ï¿½m
         speech_recognizer.stop();
     }
 
-    // ??nh d?ng ngôn ng? ??u ra c?a máy
+    // ??nh d?ng ngï¿½n ng? ??u ra c?a mï¿½y
     speech_utterance.lang = selLang.value;
 
-    // ??nh d?ng n?i dung mà máy s? phát
+    // ??nh d?ng n?i dung mï¿½ mï¿½y s? phï¿½t
     speech_utterance.text = s;
 
-    // Máy ti?n hành nói
+    // Mï¿½y ti?n hï¿½nh nï¿½i
     window.speechSynthesis.speak(speech_utterance);
 }
 
 
-// Nút t?t âm
+// Nï¿½t t?t ï¿½m
 function Mute(b) {
     if (b) {
         selVoices.style.display = "none";
@@ -236,11 +236,11 @@ function Mute(b) {
 }
 
 
-// Hàm chuy?n  âm thanh ??u vào thành v?n b?n
+// Hï¿½m chuy?n  ï¿½m thanh ??u vï¿½o thï¿½nh v?n b?n
 function SpeechToText() {
-    // N?u trình nh?n d?ng gi?ng nói ?ang ???c b?t
+    // N?u trï¿½nh nh?n d?ng gi?ng nï¿½i ?ang ???c b?t
     if (speech_recognizer) {
-        // Ghi âm n?u nút microphone ?ang b?t, và ng?ng ghi âm n?u nút ???c t?t ?i
+        // Ghi ï¿½m n?u nï¿½t microphone ?ang b?t, vï¿½ ng?ng ghi ï¿½m n?u nï¿½t ???c t?t ?i
         if (chkSpeak.checked) {
             speech_recognizer.start();
         }
@@ -251,44 +251,44 @@ function SpeechToText() {
     }
 
 
-    // Kh?i t?o m?t ??i t??ng thu?c v? API nh?n d?ng gi?ng nói c?a trình duy?t 
+    // Kh?i t?o m?t ??i t??ng thu?c v? API nh?n d?ng gi?ng nï¿½i c?a trï¿½nh duy?t 
     speech_recognizer = new webkitSpeechRecognition();
-    speech_recognizer.continuous = true; // Nh?n d?ng cho ??n khi d?ng thì thôi
-    speech_recognizer.interimResults = true; // Nh?n d?ng ngay khi ng??i dùng ?ang nói
-    speech_recognizer.lang = selLang.value; // Ngôn ng? ??u vào
+    speech_recognizer.continuous = true; // Nh?n d?ng cho ??n khi d?ng thï¿½ thï¿½i
+    speech_recognizer.interimResults = true; // Nh?n d?ng ngay khi ng??i dï¿½ng ?ang nï¿½i
+    speech_recognizer.lang = selLang.value; // Ngï¿½n ng? ??u vï¿½o
     speech_recognizer.start(); // B?t ??u nh?n d?ng
 
 
-    // ??nh ngh?a hành vi khi sinh ra k?t qu?
+    // ??nh ngh?a hï¿½nh vi khi sinh ra k?t qu?
     speech_recognizer.onresult = function (event) {
         var interimTranscripts = ""; // Bi?n t?m l?u v?n b?n ??u ra
         for (var i = event.resultIndex; i < event.results.length; i++) // Duy?t t? ??u ??n cu?i c?a m?ng k?t qu?
         {
             var transcript = event.results[i][0].transcript; // L?y k?t qu? t?i ?u nh?t
 
-            if (event.results[i].isFinal) // N?u ?ã duy?t h?t
+            if (event.results[i].isFinal) // N?u ?ï¿½ duy?t h?t
             {
-                // Truy?n toàn b? k?t qu? nh?n di?n vào chat-input
+                // Truy?n toï¿½n b? k?t qu? nh?n di?n vï¿½o chat-input
                 $("#chat-input").val(transcript);
-                // Truy?n thông ?i?p vào yêu c?u ?? g?i lên API c?a OpenAI
+                // Truy?n thï¿½ng ?i?p vï¿½o yï¿½u c?u ?? g?i lï¿½n API c?a OpenAI
                 Send();
             }
             else // Ch?a duy?t h?t
             {
-                // ??nh d?ng xu?ng dòng ?? hi?n th? xu?ng dòng trong html
+                // ??nh d?ng xu?ng dï¿½ng ?? hi?n th? xu?ng dï¿½ng trong html
                 transcript.replace("\n", "<br>");
-                // Ch?ng k?t qu? lên nhau 
+                // Ch?ng k?t qu? lï¿½n nhau 
                 interimTranscripts += transcript;
             }
 
 
-            // K?t qu? t?m th?i c?a thông ?i?p thu âm ???c nh?n d?ng
+            // K?t qu? t?m th?i c?a thï¿½ng ?i?p thu ï¿½m ???c nh?n d?ng
             var oDiv = document.getElementById("idText");
             oDiv.innerHTML = '<span style="color: #999;">' + interimTranscripts + '</span>';
         }
     };
 
-    // N?u có l?i x?y ra, không làm gì c?
+    // N?u cï¿½ l?i x?y ra, khï¿½ng lï¿½m gï¿½ c?
     speech_recognizer.onerror = function (event) {
 
     };
